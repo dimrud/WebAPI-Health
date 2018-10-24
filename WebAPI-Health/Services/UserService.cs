c6e1c1cc211cf130926422a260ceb3b245c5a9e0
Dimitry Rudoy <dimitry_rudoy@dealer-fx.com>
2018-10-24 12:09:53 -0400

    Fix moderate severity security vulnerability detected in Microsoft.AspNetCore.All Compilation errors

--- WebAPI-Health/Services/UserService.cs
+++ WebAPI-Health/Services/UserService.cs
@@ -10,7 +10,7 @@ using WebAPIHealth.Models.WebApiModels;
 
 namespace WebAPIHealth.Services
 {
-	public class UserService : IUserService
+	public class UserService //: IUserService
 	{
 		private WebApiModels _context;
 
@@ -19,130 +19,130 @@ namespace WebAPIHealth.Services
 			_context = context;
 		}
 
-		public User Authenticate(string username, string password)
-		{
-			if (string.IsNullOrEmpty(username) || string.IsNullOrEmpty(password))
-				return null;
+		//public User Authenticate(string username, string password)
+		//{
+		//	if (string.IsNullOrEmpty(username) || string.IsNullOrEmpty(password))
+		//		return null;
 
-			var user = _context.AspNetUsers.SingleOrDefault(x => x.UserName == username);
+		//	var user = _context.AspNetUsers.SingleOrDefault(x => x.UserName == username);
 
-			// check if username exists
-			if (user == null)
-				return null;
+		//	// check if username exists
+		//	if (user == null)
+		//		return null;
 
-			// check if password is correct
-			//if (!VerifyPasswordHash(password, user.PasswordHash, user.PasswordSalt))
-			//	return null;
+		//	// check if password is correct
+		//	//if (!VerifyPasswordHash(password, user.PasswordHash, user.PasswordSalt))
+		//	//	return null;
 
-			// authentication successful
-			return user;
-		}
+		//	// authentication successful
+		//	return user;
+		//}
 
-		public IEnumerable<User> GetAll()
-		{
-			return _context.Users;
-		}
+		//public IEnumerable<User> GetAll()
+		//{
+		//	return _context.Users;
+		//}
 
-		public User GetById(int id)
-		{
-			return _context.Users.Find(id);
-		}
+		//public User GetById(int id)
+		//{
+		//	return _context.Users.Find(id);
+		//}
 
-		public User Create(User user, string password)
-		{
-			// validation
-			if (string.IsNullOrWhiteSpace(password))
-				throw new AppException("Password is required");
+		//public User Create(User user, string password)
+		//{
+		//	// validation
+		//	if (string.IsNullOrWhiteSpace(password))
+		//		throw new AppException("Password is required");
 
-			if (_context.Users.Any(x => x.Username == user.Username))
-				throw new AppException("Username \"" + user.Username + "\" is already taken");
+		//	if (_context.Users.Any(x => x.Username == user.Username))
+		//		throw new AppException("Username \"" + user.Username + "\" is already taken");
 
-			byte[] passwordHash, passwordSalt;
-			CreatePasswordHash(password, out passwordHash, out passwordSalt);
+		//	byte[] passwordHash, passwordSalt;
+		//	CreatePasswordHash(password, out passwordHash, out passwordSalt);
 
-			user.PasswordHash = passwordHash;
-			user.PasswordSalt = passwordSalt;
+		//	user.PasswordHash = passwordHash;
+		//	user.PasswordSalt = passwordSalt;
 
-			_context.Users.Add(user);
-			_context.SaveChanges();
+		//	_context.Users.Add(user);
+		//	_context.SaveChanges();
 
-			return user;
-		}
+		//	return user;
+		//}
 
-		public void Update(User userParam, string password = null)
-		{
-			var user = _context.Users.Find(userParam.Id);
+		//public void Update(User userParam, string password = null)
+		//{
+		//	var user = _context.Users.Find(userParam.Id);
 
-			if (user == null)
-				throw new AppException("User not found");
+		//	if (user == null)
+		//		throw new AppException("User not found");
 
-			if (userParam.Username != user.Username)
-			{
-				// username has changed so check if the new username is already taken
-				if (_context.Users.Any(x => x.Username == userParam.Username))
-					throw new AppException("Username " + userParam.Username + " is already taken");
-			}
+		//	if (userParam.Username != user.Username)
+		//	{
+		//		// username has changed so check if the new username is already taken
+		//		if (_context.Users.Any(x => x.Username == userParam.Username))
+		//			throw new AppException("Username " + userParam.Username + " is already taken");
+		//	}
 
-			// update user properties
-			user.FirstName = userParam.FirstName;
-			user.LastName = userParam.LastName;
-			user.Username = userParam.Username;
+		//	// update user properties
+		//	user.FirstName = userParam.FirstName;
+		//	user.LastName = userParam.LastName;
+		//	user.Username = userParam.Username;
 
-			// update password if it was entered
-			if (!string.IsNullOrWhiteSpace(password))
-			{
-				byte[] passwordHash, passwordSalt;
-				CreatePasswordHash(password, out passwordHash, out passwordSalt);
+		//	// update password if it was entered
+		//	if (!string.IsNullOrWhiteSpace(password))
+		//	{
+		//		byte[] passwordHash, passwordSalt;
+		//		CreatePasswordHash(password, out passwordHash, out passwordSalt);
 
-				user.PasswordHash = passwordHash;
-				user.PasswordSalt = passwordSalt;
-			}
+		//		user.PasswordHash = passwordHash;
+		//		user.PasswordSalt = passwordSalt;
+		//	}
 
-			_context.Users.Update(user);
-			_context.SaveChanges();
-		}
+		//	_context.Users.Update(user);
+		//	_context.SaveChanges();
+		//}
 
-		public void Delete(int id)
-		{
-			var user = _context.Users.Find(id);
-			if (user != null)
-			{
-				_context.Users.Remove(user);
-				_context.SaveChanges();
-			}
-		}
+		//public void Delete(int id)
+		//{
+		//	var user = _context.Users.Find(id);
+		//	if (user != null)
+		//	{
+		//		_context.Users.Remove(user);
+		//		_context.SaveChanges();
+		//	}
+		//}
 
-		// private helper methods
+		//// private helper methods
 
-		private static void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
-		{
-			if (password == null) throw new ArgumentNullException("password");
-			if (string.IsNullOrWhiteSpace(password)) throw new ArgumentException("Value cannot be empty or whitespace only string.", "password");
+		//private static void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
+		//{
+		//	if (password == null) throw new ArgumentNullException("password");
+		//	if (string.IsNullOrWhiteSpace(password)) throw new ArgumentException("Value cannot be empty or whitespace only string.", "password");
 
-			using (var hmac = new System.Security.Cryptography.HMACSHA512())
-			{
-				passwordSalt = hmac.Key;
-				passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
-			}
-		}
+		//	using (var hmac = new System.Security.Cryptography.HMACSHA512())
+		//	{
+		//		passwordSalt = hmac.Key;
+		//		passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+		//	}
+		//}
 
-		private static bool VerifyPasswordHash(string password, byte[] storedHash, byte[] storedSalt)
-		{
-			if (password == null) throw new ArgumentNullException("password");
-			if (string.IsNullOrWhiteSpace(password)) throw new ArgumentException("Value cannot be empty or whitespace only string.", "password");
-			if (storedHash.Length != 64) throw new ArgumentException("Invalid length of password hash (64 bytes expected).", "passwordHash");
-			if (storedSalt.Length != 128) throw new ArgumentException("Invalid length of password salt (128 bytes expected).", "passwordHash");
+		//private static bool VerifyPasswordHash(string password, byte[] storedHash, byte[] storedSalt)
+		//{
+		//	if (password == null) throw new ArgumentNullException("password");
+		//	if (string.IsNullOrWhiteSpace(password)) throw new ArgumentException("Value cannot be empty or whitespace only string.", "password");
+		//	if (storedHash.Length != 64) throw new ArgumentException("Invalid length of password hash (64 bytes expected).", "passwordHash");
+		//	if (storedSalt.Length != 128) throw new ArgumentException("Invalid length of password salt (128 bytes expected).", "passwordHash");
 
-			using (var hmac = new System.Security.Cryptography.HMACSHA512(storedSalt))
-			{
-				var computedHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
-				for (int i = 0; i < computedHash.Length; i++)
-				{
-					if (computedHash[i] != storedHash[i]) return false;
-				}
-			}
+		//	using (var hmac = new System.Security.Cryptography.HMACSHA512(storedSalt))
+		//	{
+		//		var computedHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+		//		for (int i = 0; i < computedHash.Length; i++)
+		//		{
+		//			if (computedHash[i] != storedHash[i]) return false;
+		//		}
+		//	}
 
-			return true;
-		}
+		//	return true;
+		//}
 	}
 }
